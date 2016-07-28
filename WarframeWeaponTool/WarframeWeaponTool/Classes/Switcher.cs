25b145fe73bd6d67a34ec684820ea89781c1d228
Gazza <corey.uren@outlook.com.au>
2016-07-28 22:08:43 +1000

    Began final compilation of DRAFTs into one application. TO DO: Add respective page codes.

---
+++ WarframeWeaponTool/WarframeWeaponTool/Classes/Switcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WarframeWeaponTool.Pages;
+
+namespace WarframeWeaponTool.Classes
+{
+    public static class Switcher
+    {
+        public static MainWindow mainWindow;
+
+        public static void Switch(UserControl newPage)
+        {
+            mainWindow.Navigate(newPage);
+        }
+
+        public static void Switch(UserControl newPage, object state)
+        {
+            mainWindow.Navigate(newPage, state);
+        }
+
+
+    }
+}
---
+++ WarframeWeaponTool/WarframeWeaponTool/Pages/inventory.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WarframeWeaponTool.Classes;
+
+namespace WarframeWeaponTool.Pages
+{
+    /// <summary>
+    /// Interaction logic for weapons.xaml
+    /// </summary>
+    public partial class inventory : UserControl, ISwitchable
+    {
+        public inventory()
+        {
+            InitializeComponent();
+        }
+        int screenIndex = 3;
+
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void menuSelectLoaded(object sender, RoutedEventArgs e)
+        {
+            var menu = sender as ComboBox;
+            menu.SelectedIndex = screenIndex;
+            menu.ItemsSource = sharedMethods.createMenu(sender);
+        }
+
+        private void menuSelectSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var menu = sender as ComboBox;
+            int selectedIndex = menu.SelectedIndex;
+            int currentIndex = screenIndex;
+            sharedMethods.MenuSelect(sender, selectedIndex, currentIndex);
+        }
+    }
+
+}
